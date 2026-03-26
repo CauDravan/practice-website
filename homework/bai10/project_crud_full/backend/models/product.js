a14f876bd1e07e6489e5dd4a7a81496f481873be
CauDravan <kindgate000@gmail.com>
2026-03-26 16:23:47 +0700

    remove unnessesary file

--- homework/bai10/project_crud_full/backend/models/product.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    price: { type: DataTypes.FLOAT, allowNull: false },
-    image: { type: DataTypes.STRING, allowNull: true }
-  }, {});
-  return Product;
-};
